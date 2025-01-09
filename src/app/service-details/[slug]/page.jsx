b92a659f2d88998978fc675a793ccf18b42ef7cb
Leta Cheru <letacheru484@gmail.com>
2025-01-09 05:23:55 -0900

    style

--- src/app/service-details/[slug]/page.jsx
+++ src/app/service-details/[slug]/page.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import Services2Data from '@/assets/jsonData/services/Services2Data.json'
+import Services2Data from '@/assets/jsonData/services/Services2Data.json';
 import BreadCrumb from '@/components/breadCrumb/BreadCrumb';
 import ServiceDetails from '@/components/pages/service-details/ServiceDetails';
 
-export const metadata = {
-    title: "Consua - Consulting Business - Services Details"
-}
-
 const ServiceDetailsPage = ({ params }) => {
-
-    const { slug } = params
-    const data = Services2Data.find(service => service.id === parseInt(slug))
+    const { slug } = params;
+    const selectedService = Services2Data.find(service => service.id === parseInt(slug));
 
     return (
         <>
             <BreadCrumb breadCrumb="Service-details" title="Service Details" />
-                <ServiceDetails serviceInfo={data} />
-            
+            <ServiceDetails serviceInfo={selectedService} allServices={Services2Data} />
         </>
     );
 };
